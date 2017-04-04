26178db4360702dbaa0d5505f74144b4dd9eccfe
Savanna-Rae Furtah <savannarae@live.ca>
2017-04-04 09:43:48 -0400

    Added NewHire model and controller

---
+++ CSDHRProject/Controllers/NewHireController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDHRProject.Controllers
+{
+    public class NewHireController : Controller
+    {
+        // GET: NewHire
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NewHire/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewHire/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewHire/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewHire/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewHire/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewHire/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewHire/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CSDHRProject/Models/NewHireModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class NewHireModels
+    {
+        public String Name { get; set; }
+        public String Id { get; set; }
+        public String Sin { get; set; }
+        public String BenefitNumber {get;set;}
+        public int RateOfPay { get; set; }       
+        public int VacationDays { get; set; }
+        public int SickDays { get; set; }
+        public String BenefitCertificateFileName { get; set; }
+        public String TrainingCertificateFileName { get; set; }
+    }
+        public class NewHireEditViewModel
+    {
+        public HttpPostedFileBase BenefitFile { get; set; }
+        public HttpPostedFileBase TrainingFile { get; set; }
+    }
+    
+}
