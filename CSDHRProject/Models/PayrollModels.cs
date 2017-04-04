783826ab03984747f9405afd22f9d6742e015711
Chance Cooper <c0659824@mylambton.ca>
2017-04-04 09:48:18 -0400

    Renamed PayrollViewModels to PayrollModels and added some getters and setters

---
+++ CSDHRProject/Models/PayrollModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class PayrollModels
+    {
+        public int EmployeeId { get; set; }
+        public String EmployeeName { get; set; }
+        public int ManagerId { get; set; }
+        public String ManagerName { get; set; }
+        public double PayRate { get; set; }
+
+    }
+}
