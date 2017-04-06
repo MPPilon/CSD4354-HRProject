4f2b16064c963741da23f36b455f1316b03dc20b
Michael Snopko <michael.snopko@gmail.com>
2017-04-06 08:30:45 -0400

    Added Models Employee Claim

---
+++ CSDHRProject/Models/EmployeeClaimModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class EmployeeClaim
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public virtual Address Address { get; set; }
+        public Double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public String Service { get; set; }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+    public String Street { get; set; }
+    public String City { get; set; }
+    public String Provence { get; set; }
+    public String Postal { get; set; }
+    }
+}
