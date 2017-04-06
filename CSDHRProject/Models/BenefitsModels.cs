4ed89c7c68af3f0344e5dded6c10e3a478805d12
Tyler Drury <c0661137@mylambton.ca>
2017-04-06 09:28:38 -0400

    Finished the Benefits model. Created an indx page.

--- CSDHRProject/Models/BenefitsModels.cs
+++ CSDHRProject/Models/BenefitsModels.cs
@@ -18,9 +18,24 @@ namespace CSDHRProject.Models
     public class Address
     {
         public int Id { get; set; }
-    public String Street { get; set; }
-    public String City { get; set; }
-    public String Provence { get; set; }
-    public String Postal { get; set; }
+        public String Street { get; set; }
+        public String City { get; set; }
+        public String Provence { get; set; }
+        public String Postal { get; set; }
+    }
+
+    public class BenefitRegistration
+    {
+        public int Id { get; set; }
+        public Boolean Single { get; set; }
+        public Boolean Family { get; set; }
+        public Boolean Standard { get; set; }
+        public Boolean Extended { get; set; }
+        public Boolean Dental { get; set; }
+        public Boolean DentalNone { get; set; }
+        public Boolean Optical { get; set; }
+        public Boolean OpticalNone { get; set; }
+        public Boolean Paramedical { get; set; }
+        public Boolean ParamedicalNone { get; set; }
     }
 }
