188d7c23eb78fdc8579129ef05a69362c3cf4b5b
Jamey Wallace <c0660455@mylambton.ca>
2017-04-06 08:37:24 -0400

    Added the models (object of change )

---
+++ CSDHRProject/Models/Project Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class Project_Model
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public  DateTime Date { get; set; }
+        public String Author { get; set; }
+        public String Lead { get; set; }
+
+
+
+    }
+
+    public class Project_user
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public bool Manager { get; set; }
+
+    }
+}
