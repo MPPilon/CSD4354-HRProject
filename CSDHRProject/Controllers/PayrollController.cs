3526e455cc8680e656a28da3945ffbdfdeaa2389
Kelsey Bruin <Aerrow93>
2017-04-09 13:33:50 -0400

    Added Return Home buttons to all the pages

--- CSDHRProject/Controllers/PayrollController.cs
+++ CSDHRProject/Controllers/PayrollController.cs
@@ -208,17 +208,18 @@ namespace CSDHRProject.Controllers
             return View(payrollModels);
         }
 
-        public ActionResult LoadProjects()
-        {
-            List<SelectListItem> li = new List<SelectListItem>();
-            li.Add(new SelectListItem { Text = "Select Project", Value = 0 });
-            foreach (var project in db.PayrollModels.Project)
-            {
-                var count = 1;
-                li.Add(new SelectListItem { Text = Model.ProjectName, Value = count });
-                count++;
-            }
-        }
+        //Attempt at interation of Project Dropdownlist. Found that DisplayFor or EditorFor is a better use as it does it as part of it's structure
+        //public ActionResult LoadProjects()
+        //{
+        //    List<SelectListItem> li = new List<SelectListItem>();
+        //    li.Add(new SelectListItem { Text = "Select Project", Value = 0 });
+        //    foreach (var project in db.PayrollModels.Project)
+        //    {
+        //        var count = 1;
+        //        li.Add(new SelectListItem { Text = Model.ProjectName, Value = count });
+        //        count++;
+        //    }
+        //}
         #endregion
 
         #region EmployeeTimesheet
