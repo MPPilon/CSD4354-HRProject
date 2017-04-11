6689a84268edf4a7c55f8eafaf93f4eb9c9fbab2
Savanna-Rae Furtah <savannarae@live.ca>
2017-04-11 10:01:23 -0400

    Created viewModel for new hire to include account creation and newHire views

---
+++ CSDHRProject/Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class ViewModel
+    {
+        public NewHireModel NewHire { get;set;}
+        public RegisterViewModel Account { get; set; }
+    }
+}
