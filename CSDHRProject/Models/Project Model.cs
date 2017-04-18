0c763d6ccb463da993359dbb6b79e38d0a336b00
Jamey Wallace <c0660455@mylambton.ca>
2017-04-18 08:40:57 -0400

    added manager

---
+++ CSDHRProject/Models/Project Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSDHRProject.Models
+{
+    public class Project_Model
+    {
+        public int  Id { get; set;}
+        public DateTime Date { get; set; }
+        public String Name { get; set; }
+       
+
+        public class projectusers
+        {
+            public int Id { get; set; }
+            public String lead { get; set; }
+            public bool manager { get; set; }
+        }
+
+
+    }
+}
