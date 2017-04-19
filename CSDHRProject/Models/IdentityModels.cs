133633f5b4349390a65463862ed9ca6148324f40
Matthew Pilon <mppilon@gmail.com>
2017-04-19 16:17:30 -0400

    Update IdentityModels.cs
    I guess people are already using an admin property so I'm sticking that back in

--- CSDHRProject/Models/IdentityModels.cs
+++ CSDHRProject/Models/IdentityModels.cs
@@ -19,7 +19,7 @@ namespace CSDHRProject.Models
 
         //Custom User attributes
         public bool manager { get; set; }
-        public bool role { get; set; }
+        public bool admin { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
     }
