639a4e8af9e233e35fe4fde73a23271e29a1f81c
Savanna-Rae Furtah <savannarae@live.ca>
2017-04-19 16:34:58 -0400

    Revert "Merge branch 'testmerge' into NewHire"
    This reverts commit c4b9e7c5443c413d7d36930e413ac341421c826d, reversing
    changes made to b5c11702d9a528084fbbf448505e790389af848c.

--- CSDHRProject/Models/IdentityModels.cs
+++ CSDHRProject/Models/IdentityModels.cs
@@ -50,9 +50,6 @@ namespace CSDHRProject.Models
 
         public System.Data.Entity.DbSet<CSDHRProject.Models.RegisterViewModel> NewHireModels { get; set; }
         
-
-        public System.Data.Entity.DbSet<CSDHRProject.Models.Project> Projects { get; set; }
-        public System.Data.Entity.DbSet<CSDHRProject.Models.ProjectUser> ProjectUsers { get; set; }
         public System.Data.Entity.DbSet<CSDHRProject.Models.BenefitRegistration> BenefitRegistrations { get; set; }
 
         public System.Data.Entity.DbSet<CSDHRProject.Models.EmployeeClaim> EmployeeClaims { get; set; }
